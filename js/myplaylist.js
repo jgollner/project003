663abca84fb154da7faa0f7eaba4866f7b159522
jgollner <jamesgollner@gmail.com>
2017-06-21 10:42:18 -0700

    BoomBox

---
+++ js/myplaylist.js
@@ -0,0 +1,24 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'SLATS SLATS SLATS',
+        artist:'SKRILLEX',
+        rating:4,
+        cover:'mix/1.png'
+    }
+
+
+
+   
+]
+
